c852a2017cff34a0b50e5b46c46601d5f377dad0
nissi1234 <150512493+nissi1234@users.noreply.github.com>
2024-03-05 17:52:43 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Creating EC2 Instance
 resource "aws_instance" "wordpress" {
-  ami                         = "ami-0440d3b780d96b29d"
+  ami                         = "ami-07761f3ae34c4478d"
   instance_type               = "t2.micro"
-  key_name                    = "wordpress1"
+  key_name                    = "python-jenkins"
   vpc_security_group_ids      = [aws_security_group.ec2-SG.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
