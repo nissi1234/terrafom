9e58ce7774177b44e1cc78214b23674190c179ad
nishwanth raju <150512493+nissi1234@users.noreply.github.com>
2024-03-19 16:57:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
 # Creating EC2 Instance
 resource "aws_instance" "wordpress" {
-  ami                         = "ami-07761f3ae34c4478d"
+  ami                         = "ami-02d7fd1c2af6eead0"
   instance_type               = "t2.micro"
-  key_name                    = "python-jenkins"
+  key_name                    = "master1"
   vpc_security_group_ids      = [aws_security_group.ec2-SG.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
   user_data                   = file("userdata.sh")
 
   tags = {
-    Name = "Wordpress"
+    Name = "terraform"
   }
 }
