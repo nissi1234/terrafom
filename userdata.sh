e39669132140d64d46472c35074e96dfe9881c0d
nishwanth raju <150512493+nissi1234@users.noreply.github.com>
2024-03-23 22:05:06 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sudo yum -y install git 
-git clone https://github.com/nissi1234/Agri.git
-cd Agri/
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
+sudo yum -y install docker 
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/nissi1234/wordpress.git
+cd wordpress
+docker-compose up -d
